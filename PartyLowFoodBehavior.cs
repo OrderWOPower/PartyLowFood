c931fd588358cefd86ba5fa4114278b1c15458c0
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2021-07-13 21:35:21 +0800

    Add files via upload

---
+++ PartyLowFoodBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+
+namespace PartyLowFood
+{
+    public class PartyLowFoodBehavior : FoodConsumptionBehavior
+    {
+        public override void RegisterEvents() => CampaignEvents.DailyTickPartyEvent.AddNonSerializedListener(this, new Action<MobileParty>(PartyLowFoodWarn));
+        // Display a warning message when the player's party has less than or equal to 3 days of food left.
+        private void PartyLowFoodWarn(MobileParty mobileParty)
+        {
+            float num = (mobileParty.Food > 0f) ? mobileParty.Food : 0f;
+            int num2 = (int)Math.Ceiling(Math.Abs(num / mobileParty.FoodChange));
+            if (mobileParty.IsMainParty)
+            {
+                MBTextManager.SetTextVariable("REMAINING_FOOD", num2);
+                MBTextManager.SetTextVariable("PLURAL", (num2 > 1) ? 1 : 0);
+                if (num2 <= 3 && num2 > 0)
+                {
+                    InformationManager.AddQuickInformation(new TextObject("Your party is low on food. You have {REMAINING_FOOD} {?PLURAL}days{?}day{\\?} until no food.", null), 0, null, "");
+                }
+            }
+        }
+    }
+}
---
+++ PartyLowFoodSubModule.cs
@@ -0,0 +1,19 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace PartyLowFood
+{
+    // This mod warns the player when their party is low on food.
+    public class PartyLowFoodSubModule : MBSubModuleBase
+    {
+        protected override void OnGameStart(Game game, IGameStarter gameStarter)
+        {
+            if (game.GameType is Campaign)
+            {
+                CampaignGameStarter campaignStarter = (CampaignGameStarter)gameStarter;
+                campaignStarter.AddBehavior(new PartyLowFoodBehavior());
+            }
+        }
+    }
+}
