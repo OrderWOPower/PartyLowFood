5867743f2729f5eb0ad5689414e220215821f235
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2021-12-03 20:42:51 +0800

    Refactored the code.

--- PartyLowFoodBehavior.cs
+++ PartyLowFoodBehavior.cs
@@ -8,20 +8,24 @@ namespace PartyLowFood
 {
     public class PartyLowFoodBehavior : FoodConsumptionBehavior
     {
-        public override void RegisterEvents() => CampaignEvents.DailyTickPartyEvent.AddNonSerializedListener(this, new Action<MobileParty>(PartyLowFoodWarn));
+        public override void RegisterEvents() => CampaignEvents.DailyTickPartyEvent.AddNonSerializedListener(this, new Action<MobileParty>(OnDailyTickParty));
+        public void OnDailyTickParty(MobileParty mobileParty)
+        {
+            if (mobileParty.IsMainParty)
+            {
+                PartyLowFoodWarn(mobileParty);
+            }
+        }
         // Display a warning message when the player's party has less than or equal to 3 days of food left.
         private void PartyLowFoodWarn(MobileParty mobileParty)
         {
             float num = (mobileParty.Food > 0f) ? mobileParty.Food : 0f;
             int num2 = (int)Math.Ceiling(Math.Abs(num / mobileParty.FoodChange));
-            if (mobileParty.IsMainParty)
+            MBTextManager.SetTextVariable("REMAINING_FOOD", num2);
+            MBTextManager.SetTextVariable("PLURAL", (num2 > 1) ? 1 : 0);
+            if (num2 <= 3 && num2 > 0)
             {
-                MBTextManager.SetTextVariable("REMAINING_FOOD", num2);
-                MBTextManager.SetTextVariable("PLURAL", (num2 > 1) ? 1 : 0);
-                if (num2 <= 3 && num2 > 0)
-                {
-                    InformationManager.AddQuickInformation(new TextObject("Your party is low on food. You have {REMAINING_FOOD} {?PLURAL}days{?}day{\\?} until no food.", null), 0, null, "");
-                }
+                InformationManager.AddQuickInformation(new TextObject("Your party is low on food. You have {REMAINING_FOOD} {?PLURAL}days{?}day{\\?} until no food.", null), 0, null, "");
             }
         }
     }
