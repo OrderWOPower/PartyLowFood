80b434fd261b785a2c56be7979e0fc2e9bdcfaca
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2022-03-24 19:32:42 +0800

    Updated for Bannerlord 1.7.2.

--- PartyLowFoodBehavior.cs
+++ PartyLowFoodBehavior.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using TaleWorlds.CampaignSystem;
-using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors;
+using TaleWorlds.CampaignSystem.CampaignBehaviors;
+using TaleWorlds.CampaignSystem.Party;
 using TaleWorlds.Core;
 using TaleWorlds.Localization;
 
@@ -9,23 +10,20 @@ namespace PartyLowFood
     public class PartyLowFoodBehavior : FoodConsumptionBehavior
     {
         public override void RegisterEvents() => CampaignEvents.DailyTickPartyEvent.AddNonSerializedListener(this, new Action<MobileParty>(OnDailyTickParty));
-        public void OnDailyTickParty(MobileParty mobileParty)
-        {
-            if (mobileParty.IsMainParty)
-            {
-                PartyLowFoodWarn(mobileParty);
-            }
-        }
+
         // Display a warning message when the player's party has less than or equal to 3 days of food left.
-        private void PartyLowFoodWarn(MobileParty mobileParty)
+        private void OnDailyTickParty(MobileParty party)
         {
-            float num = (mobileParty.Food > 0f) ? mobileParty.Food : 0f;
-            int num2 = (int)Math.Ceiling(Math.Abs(num / mobileParty.FoodChange));
-            MBTextManager.SetTextVariable("REMAINING_FOOD", num2);
-            MBTextManager.SetTextVariable("PLURAL", (num2 > 1) ? 1 : 0);
-            if (num2 <= 3 && num2 > 0)
+            if (party.IsMainParty)
             {
-                InformationManager.AddQuickInformation(new TextObject("Your party is low on food. You have {REMAINING_FOOD} {?PLURAL}days{?}day{\\?} until no food.", null), 0, null, "");
+                float num = (party.Food > 0f) ? party.Food : 0f;
+                int num2 = (int)Math.Ceiling(Math.Abs(num / party.FoodChange));
+                MBTextManager.SetTextVariable("REMAINING_FOOD", num2);
+                MBTextManager.SetTextVariable("PLURAL", (num2 > 1) ? 1 : 0);
+                if (num2 <= 3 && num2 > 0)
+                {
+                    InformationManager.AddQuickInformation(new TextObject("Your party is low on food. You have {REMAINING_FOOD} {?PLURAL}days{?}day{\\?} until no food.", null), 0, null, "");
+                }
             }
         }
     }
